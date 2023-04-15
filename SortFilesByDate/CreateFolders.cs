511640a4280ae60c30a2ecee6e5fabd64a351273
Magic146W <macwer975@gmail.com>
2023-04-15 17:24:30 +0200

    Add project files.

---
+++ SortFilesByDate/CreateFolders.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortFilesByDate
+{
+    class CreateFolders
+    {
+        private static int myDecission;
+        public int MyDecission { get { return myDecission; } set { myDecission = value; } }
+
+        private static string myPathToSort;
+        public string MyPathToSort { get { return myPathToSort; } set { myPathToSort = value; } }
+
+        private static string myPathNewFolder;
+
+        private static List<string> _files = new List<string>();
+        private static string _dateFormat;
+
+        public void StartSorting()
+        {
+            _dateFormat = TimeFormat();
+            CreateFoldersAndSortItems();
+        }
+
+        private static void CreateFoldersAndSortItems()
+        {
+            for (int i = 0; i < _files.Count; i++)
+            {
+                string monthFolderName = File.GetLastWriteTime(Path.Combine(myPathToSort, _files[i])).ToString(_dateFormat);
+                string monthFolderPath = Path.Combine(myPathNewFolder, monthFolderName);
+
+                if (!Directory.Exists(monthFolderPath))
+                {
+                    Directory.CreateDirectory(monthFolderPath);
+                }
+
+                File.Move(Path.Combine(myPathToSort, Path.GetFileName(_files[i])), Path.Combine(monthFolderPath, Path.GetFileName(_files[i])));
+            }
+        }
+
+        private static string TimeFormat()
+        {
+            if (myDecission == 3)
+            {
+                return "yyyy-MM-dd";
+            }
+            else if (myDecission == 2)
+            {
+                return "yyyy-MM";
+            }
+            else
+            {
+                return "yyyy";
+            }
+        }
+
+        public bool FolderSelection(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path);
+                _files = files.ToList<string>();
+
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"Folder doesn't exist.");
+                return false;
+            }
+        }
+
+        public bool FolderCreation(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                myPathNewFolder = Path.Combine(path, "SortedFiles");
+                Directory.CreateDirectory(myPathNewFolder);
+
+                return true;
+            }
+            else
+            {
+                myPathNewFolder = Path.Combine(myPathToSort, "SortedFiles");
+                Directory.CreateDirectory(myPathNewFolder);
+
+                return true;
+            }
+        }
+
+        public CreateFolders()
+        { }
+    }
+}
---
+++ SortFilesByDate/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+
+namespace SortFilesByDate
+{
+    class Program
+    {
+        static CreateFolders _createFolders = new CreateFolders();
+        private static void Main(string[] args)
+        {
+            UsersFolderDecisions();
+            _createFolders.StartSorting();
+
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+
+        private static void UsersFolderDecisions()
+        {
+            string path;
+
+            Console.WriteLine("This program sorts files by their date of last modification. Creating new folders for matching files." +
+                "\nTo select a folder copy and paste it's path, then press enter.");
+            do
+            {
+                Console.Write("\n\nPath:\t");
+                path = Console.ReadLine();
+
+            } while (!_createFolders.FolderSelection(path));
+            _createFolders.MyPathToSort = path;
+            path = "";
+
+            Console.Write("\n\nTo start give path where new folder will be placed. In case of no path current path will be selected\nPath:\t");
+            do
+            {         
+                path = Console.ReadLine();
+            } while (!_createFolders.FolderCreation(path));
+
+            FolderSortingDecission();
+        }
+
+        private static void FolderSortingDecission()
+        {
+            Console.WriteLine("\n\nDecide if you want to sort by:\n[1]\t- year\n[2]\t- month\n[3]\t- day");
+            int decide = 0;
+            bool exit = false;
+            do
+            {
+                string decission = Console.ReadLine();
+                try
+                {
+                    decide = Int16.Parse(decission);
+                }
+                catch (Exception) { }
+
+                if (decide == 1 || decide == 2 || decide == 3)
+                {
+                    exit = true;
+                }
+            } while (!exit);
+
+            _createFolders.MyDecission = decide;
+        }
+    }
+}
